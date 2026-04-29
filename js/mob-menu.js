0e5e817255ab2a740657d236ef5427ccbda40d5c
Igor Nemyrovskiy <igor.nemirovskij@gmail.com>
2026-04-29 17:24:57 +0300

    add mobail menu

--- js/mob-menu.js
+++ js/mob-menu.js
@@ -10,3 +10,13 @@ mobMenuRefs.closeMenuBtn.addEventListener("click", toggleMobMenu);
 function toggleMobMenu() {
   mobMenuRefs.mobMenu.classList.toggle("is-open");
 }
+
+
+// для закриття моб-меню при натисканні на якірне посилання
+const menuLinks = document.querySelectorAll(".menu-link");
+
+menuLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    mobMenuRefs.mobMenu.classList.remove("is-open");
+  });
+});
